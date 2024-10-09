6fb815e0969c0f87155a91bf51b0e81f7fd8e4d8
Adams <Samuel.Adams@centralhealth.net>
2024-10-09 11:03:16 -0500

    created schema file and upload script

---
+++ schema.sql
@@ -0,0 +1,208 @@
+CREATE TABLE sqlite_sequence (name, seq)
+
+CREATE TABLE User (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    Username TEXT UNIQUE NOT NULL,
+    Email TEXT UNIQUE NOT NULL,
+    Password TEXT NOT NULL,
+    ImageFile BLOB NULL,
+    FirstName TEXT,
+    LastName TEXT,
+    DateCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    IsRootUser BOOLEAN NULL
+) STRICT
+
+CREATE TABLE Forecast (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    BudgetId TEXT NULL,
+    AccountNo TEXT NULL,
+    ForecastAmount REAL NULL,
+    ForecastFormula TEXT NULL,
+    Comments TEXT NULL,
+    FOREIGN KEY (BudgetId) REFERENCES Budget (BudgetId),
+    FOREIGN KEY (AccountNo) REFERENCES Account (Account)
+) STRICT
+
+CREATE TABLE ProposedBudget (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    BusinessUnitId TEXT,
+    AccountNo TEXT,
+    ProposedBudget REAL,
+    BusinessCaseName TEXT,
+    BusinessCaseAmount REAL,
+    TotalBudget REAL,
+    [Comments] TEXT,
+    FOREIGN KEY (AccountNo) REFERENCES Account (AccountNo),
+    FOREIGN KEY (BusinessUnitId) REFERENCES BusinessUnit (BusinessUnitId)
+) STRICT
+
+CREATE TABLE ProposedBusinessUnit (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    BusinessUnitId TEXT,
+    BusinessUnit TEXT
+) STRICT
+
+CREATE TABLE JournalEntry_Rad (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    JournalEntryId INTEGER,
+    RADId TEXT,
+    FOREIGN KEY (JournalEntryId) REFERENCES JournalEntry (Id),
+    FOREIGN KEY (RADId) REFERENCES RAD (RADId)
+) STRICT
+
+CREATE TABLE Budget_Rad (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    BudgetId INTEGER,
+    RADId TEXT,
+    FOREIGN KEY (BudgetId) REFERENCES Budget (Id),
+    FOREIGN KEY (RADId) REFERENCES RAD (RADId)
+) STRICT
+
+CREATE TABLE Rad (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    RADTypeId TEXT,
+    RADId TEXT,
+    RAD TEXT,
+    FOREIGN KEY (RADTypeId) REFERENCES RADType (Id)
+) STRICT
+
+CREATE TABLE RadType (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    RADTypeId TEXT UNIQUE,
+    RADType  TEXT UNIQUE
+) STRICT
+
+CREATE TABLE "Account_RadType" (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    AccountId INTEGER UNIQUE,
+    RadTypeId TEXT,
+    FOREIGN KEY (AccountId) REFERENCES Account(Id)
+) STRICT
+
+CREATE TABLE "Account" (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    "ParentAccountNo" TEXT,
+    "ChartId" INTEGER,
+    "AccountNo" TEXT UNIQUE,
+    "Account" TEXT,
+    "AccountType" TEXT,
+    "DR/CR" TEXT,
+    "PostingLevel" TEXT,
+    "InterCompanyFlag" TEXT,
+    "SecurityStatus" TEXT,
+    "Revaluation" TEXT,
+    "Reconciliation" TEXT,
+    "AccountControlClass" TEXT,
+    "ClassDescription" TEXT,
+    "ConversionTypeId" TEXT,
+    "ConversionTypeDescription" TEXT,
+    "UDF1" TEXT,
+    "XBRLTag" TEXT,
+    "UserCreated" TEXT,
+    "DateCreated" TEXT
+) STRICT
+
+CREATE TABLE JournalEntry (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    FiscalYear TEXT,
+    CompanyId TEXT,
+    EntryId TEXT,
+    BusinessUnitId TEXT,
+    AccountNo TEXT,
+    Amount REAL,
+    AccountingDate TEXT,
+    Remarks TEXT,
+    FOREIGN KEY (BusinessUnitId) REFERENCES BusinessUnit(BusinessUnitId),
+    FOREIGN KEY (AccountNo) REFERENCES Account(AccountNo),
+    FOREIGN KEY (CompanyId) REFERENCES Account(CompanyId)
+) STRICT
+
+CREATE TABLE Budget (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    FiscalYear TEXT,
+    BusinessUnitId INTEGER,
+    AccountNo TEXT,
+    Amount REAL,
+    FOREIGN KEY (AccountNo) REFERENCES Account(AccountNo),
+    FOREIGN KEY (BusinessUnitId) REFERENCES BusinessUnit(BusinessUnitId)
+) STRICT 
+
+CREATE TABLE BusinessUnit (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    BusinessUnitId TEXT,
+    BusinessUnit TEXT,
+    CompanyId TEXT,
+    Company TEXT,
+    DateCreated TEXT
+) STRICT
+
+CREATE TABLE Budget_Account (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,
+    DisplayOrder INTEGER UNIQUE NOT NULL,
+    AccountNo TEXT,
+    Account TEXT,
+    RAD TEXT,
+    CreatedDate TEXT,
+    FOREIGN KEY (AccountNo) REFERENCES Account(AccountNo),
+    FOREIGN KEY (Account) REFERENCES Account(Account),
+    FOREIGN KEY (RAD) REFERENCES RAD(RAD)
+) STRICT 
+
+CREATE VIEW vwRad_RadType AS
+SELECT 
+    rt.Id as RadTypeTableId,
+    rt."RADTypeId",
+    rt."RADType",
+    r."Id" AS RADTableId,
+    r."RADId",
+    r."RAD"
+FROM "Rad" r
+    JOIN "RadType" rt ON r."RADTypeId" = rt."RADTypeId";
+
+CREATE VIEW vwJournalEntry_By_Rad AS
+SELECT 
+    j.Id AS JournalEntryTable,
+    j.FiscalYear,
+    j.AccountNo,
+    j.Amount,
+    j.BusinessUnitId,
+    r.RadTypeId,
+    r.RadType,
+    jr.RadId,
+    r.RAD
+FROM "JournalEntry" j
+    JOIN "JournalEntry_Rad" jr ON jr."JournalEntryId" = j.Id
+    JOIN "vwRad_RadType" r ON r."RADId" = jr."RADId";
+
+CREATE VIEW vwBudget_By_Rad AS
+SELECT 
+    b.Id AS BudgetTableId,
+    b.FiscalYear,
+    b.AccountNo,
+    b.BusinessUnitId,
+    b.Amount,
+    r.RadTypeId,
+    r.RadType,
+    br.RadId,
+    r.RAD
+FROM "Budget" b
+    JOIN "Budget_Rad" br ON br."BudgetId" = b.Id
+    JOIN "vwRad_RadType" r ON r."RADId" = br."RADId";
+
+CREATE VIEW vwAccount_RadType_Rad AS 
+SELECT 
+  a.Id AS AccountTableId,
+  a."Account",
+  a."AccountNo",
+  a."AccountType",
+  a."ChartId",
+  rt."Id" AS RadTypeTableId,
+  rt."RADType",
+  rt."RADTypeId",
+  r."Id" AS RadTableId,
+  r."RADId",
+  r."RAD"
+FROM Account a
+  JOIN "Account_RadType" art ON art."AccountId" = a.Id
+  JOIN "RadType" rt ON rt."RADTypeId" = art."RadTypeId"
+  JOIN "Rad" r ON r."RADTypeId" = rt."RADTypeId";
--- upload.py
+++ upload.py
@@ -2,27 +2,385 @@ import sqlite3
 import pandas as pd
 import warnings
 import numpy as np
-from types import *
+import logging
+import os
 
-warnings.filterwarnings('ignore')
+# Configure logging
+logging.basicConfig(
+    filename="migration.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+warnings.filterwarnings("ignore")
+
 
 class DBManager:
     path: str = "main.db"
     conn: sqlite3.Connection = None
-    
+
     def __init__(self):
         pass
-    
+
     def connect(self):
-        self.conn = sqlite3.connect("main.db")
+        self.conn = sqlite3.connect(self.path)
+        logging.info("Connected to the database.")
+
+        return self.conn
+
 
 class Migration:
-    conn: sqlite3.Connection = None
-    
-    def __init__(self):
-        self.conn = DBManager().connect()
-         
-        
+    def __init__(self, db_name='main.db', schema_file='schema.sql'):
+        self.db_name = db_name
+        self.schema_file = schema_file
 
-if __name__ == "main":
-    pass
+    def create_database(self):
+        if not os.path.exists(self.db_name):
+            self.connection = sqlite3.connect(self.db_name)
+            logging.info(f"Database '{self.db_name}' created successfully.")
+            self.run_schema()
+        else:
+            logging.info(f"Database '{self.db_name}' already exists.")
+
+    def run_schema(self):
+        try:
+            with open(self.schema_file, 'r') as file:
+                schema_sql = file.read()
+                cursor = self.connection.cursor()
+                cursor.executescript(schema_sql)  # Execute multiple SQL statements
+                self.connection.commit()
+                logging.info(f"Schema from '{self.schema_file}' applied successfully.")
+        except Exception as e:
+            logging.error(f"An error occurred while applying the schema: {e}")
+        finally:
+            self.connection.close()
+
+    def import_all(self):
+        try:
+            self._import_account()
+            self._import_account_rad_type()
+            self._import_budget_rad()
+            self._import_budget()
+            self._import_budget_account()
+            self._import_journal_entry()
+            self._import_journal_entry_rad()
+            self._import_rad()
+            self._import_rad_type()
+            self._import_business_unit()
+            logging.info("All data imported successfully.")
+        except Exception as e:
+            logging.exception(f"Error during import_all: {e}")
+
+    def _import_account(self):
+        try:
+            account_raw = pd.read_csv(r"./source/Account.csv")
+            logging.info("Account data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error Account CSV: {e}")
+            return
+
+        try:
+            account_ownership_raw = pd.read_csv(r"./source/AccountOwnership.csv")
+            logging.info("Account data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error Account CSV: {e}")
+            return
+
+        account_df = account_raw.copy()
+        account_df.columns = account_df.columns.str.replace(" ", "")
+        account_df.columns = account_df.columns.str.replace("-", "")
+        account_df.dropna(inplace=True, subset="ChartId")
+        account_df["ChartId"] = account_df["ChartId"].astype(int)
+
+        account_ownership_df = account_ownership_raw.copy()
+        account_ownership_df.columns = account_ownership_df.columns.str.replace(" ", "")
+        account_ownership_df.dropna(inplace=True, subset="AccountNo")
+        account_ownership_df = account_ownership_df[["AccountNo", "ParentKeyId"]]
+        account_ownership_df["ParentKeyId"] = account_ownership_df[
+            "ParentKeyId"
+        ].str.replace("1_", "")
+        merge = pd.merge(account_df, account_ownership_df, on="AccountNo")
+        merge.rename(columns={"ParentKeyId": "ParentAccountNo"}, inplace=True)
+
+        table = "Account"
+        Util.import_func(merge, table)
+
+    def _import_account_rad_type(self):
+        try:
+            raw = pd.read_csv(r"./source/Rad_Account.csv")
+            logging.info("RAD Account data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error RAD Account CSV: {e}")
+            return
+
+        rad_account = raw.copy()
+        rad_account.columns = rad_account.columns.str.replace(" ", "")
+        rad_account["AccountNo"] = rad_account["AccountNo"].astype(str)
+
+        sql = "SELECT Id, AccountNo FROM Account;"
+        accounts = Util.execute_query(sql)
+        accounts["AccountNo"] = accounts["AccountNo"].astype(str)
+        merge = pd.merge(rad_account, accounts, on="AccountNo")
+        merge = merge[["RADTypeId", "Id"]]
+        merge = merge.rename(columns={"RADTypeId": "RadTypeId", "Id": "AccountId"})
+        merge.dropna(inplace=True)
+
+        table = "Account_RadType"
+        Util.import_func(merge, table)
+
+    def _import_budget_rad(self):
+        try:
+            raw = pd.read_csv(r"./source/budget.csv")
+            logging.info("Budget RAD data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Budget RAD CSV: {e}")
+            return
+
+        df = raw.copy()
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df_split = df.copy()
+        cols = [
+            col for col in df_split.columns if "RAD" in col and "Description" not in col
+        ]
+        df_split[cols] = df_split[cols].applymap(
+            lambda x: str(x) if not pd.isna(x) else np.nan
+        )
+        df_split["Combined"] = df_split[cols].apply(
+            lambda row: "|".join([str(x) for x in row if not pd.isna(x)]), axis=1
+        )
+        df_split = df_split.assign(value=df_split["Combined"].str.split("|")).explode(
+            "value"
+        )
+        df_result = df_split[["Id", "value"]].reset_index(drop=True)
+        df_result.replace("", float("NaN"), inplace=True)
+        df_result.dropna(inplace=True)
+        df_result.rename(columns={"value": "RADId", "Id": "BudgetId"}, inplace=True)
+
+        table = "Budget_Rad"
+        Util.import_func(df_result, table)
+
+    def _import_budget(self):
+        try:
+            raw = pd.read_csv(r"./source/Budget.csv")
+            logging.info("Budget data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Budget CSV: {e}")
+            return
+
+        df = raw.copy()
+        df.columns = df.columns.str.replace(" ", "")
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df = df.rename(columns={"BudgetId": "FiscalYear", "AccountNo.": "AccountNo"})
+        df = df[["Id", "FiscalYear", "BusinessUnitId", "AccountNo", "Amount"]]
+        df["Amount"] = df["Amount"].str.replace(",", "").fillna(0).astype(float)
+
+        table = "Budget"
+        Util.import_func(df, table)
+
+    def _import_journal_entry_rad(self):
+        try:
+            raw = pd.read_csv(r"./source/JournalEntry.csv")
+            logging.info("Journal Entry read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Journal Entry CSV: {e}")
+            return
+
+        df = raw.copy()
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df = raw.copy()
+        df.columns = df.columns.str.replace(" ", "")
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df["AccountingDate"] = pd.to_datetime(
+            df["AccountingDate"], format="%m/%d/%Y", errors="coerce"
+        )
+        df["FiscalYear"] = df["AccountingDate"].apply(Util.get_fiscal_year)
+        je = df[
+            [
+                "Id",
+                "FiscalYear",
+                "CompanyId",
+                "EntryId",
+                "BusinessUnitId",
+                "AccountNo",
+                "Amount",
+                "AccountingDate",
+                "Remarks",
+            ]
+        ].copy()
+        je["EntryId"] = je["EntryId"].fillna(0).astype(int)
+        je["AccountNo"] = (
+            je["AccountNo"].fillna(0).astype(str).replace(r"\.0$", "", regex=True)
+        )
+        je["BusinessUnitId"] = (
+            je["BusinessUnitId"].fillna(0).astype(str).replace(r"\.0$", "", regex=True)
+        )
+        je["CompanyId"] = (
+            je["CompanyId"].fillna(0).astype(str).replace(r"\.0$", "", regex=True)
+        )
+        je["Amount"] = je["Amount"].str.replace(",", "").fillna(0).astype(float)
+        
+        table = "JournalEntry"
+        Util.import_func(je, table)
+
+    def _import_journal_entry(self):
+        try:
+            raw = pd.read_csv(r"./source/JournalEntry.csv")
+            logging.info("Journal Entry read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Journal Entry CSV: {e}")
+            return
+
+        df = raw.copy()
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df = raw.copy()
+        df.columns = df.columns.str.replace(" ", "")
+        df = df.reset_index(drop=True)
+        df["Id"] = df.index + 1
+        df["AccountingDate"] = pd.to_datetime(
+            df["AccountingDate"], format="%m/%d/%Y", errors="coerce"
+        )
+        df["FiscalYear"] = df["AccountingDate"].apply(Util.get_fiscal_year)
+        df_split = df.copy()
+        cols = [
+            col for col in df_split.columns if "RAD" in col and "Description" not in col
+        ]
+
+        df_split[cols] = df_split[cols].applymap(
+            lambda x: str(x) if not pd.isna(x) else np.nan
+        )
+        df_split["Combined"] = df_split[cols].apply(
+            lambda row: "|".join([str(x) for x in row if not pd.isna(x)]), axis=1
+        )
+        df_split = df_split.assign(value=df_split["Combined"].str.split("|")).explode(
+            "value"
+        )
+        df_result = df_split[["Id", "value"]].reset_index(drop=True)
+        df_result.replace("", float("NaN"), inplace=True)
+        df_result.dropna(inplace=True)
+        df_result.rename(
+            columns={"value": "RADId", "Id": "JournalEntryId"}, inplace=True
+        )
+
+        table = "JournalEntry_Rad"
+        Util.import_func(df_result, table)
+
+    def _import_rad(self):
+        try:
+            raw = pd.read_csv(r"./source/Rad.csv")
+            logging.info("RAD data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading RAD CSV: {e}")
+            return
+
+        rad = raw.copy()
+        rad = rad[['RADTypeId', 'RADId', 'RAD']]
+        
+        table = "Rad"
+        Util.import_func(rad, table)
+
+    def _import_rad_type(self):
+        try:
+            raw = pd.read_csv(r"./source/Rad.csv")
+            logging.info("RAD data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading RAD CSV: {e}")
+            return
+
+        rad = raw.copy()
+        rad = rad[['RADTypeId', 'RADType']]
+        rad = rad.drop_duplicates()
+        
+        table = "RadType"
+        Util.import_func(rad, table)
+
+    def _import_business_unit(self):
+        try:
+            raw = pd.read_csv(r"./source/BusinessUnit.csv")
+            logging.info("Business Unit data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Business Unit CSV: {e}")
+            return
+        
+        df = raw.copy()
+        df = df.reset_index(drop=True)
+        df.columns = df.columns.str.replace(' ', "")
+        df['Id'] = df.index + 1
+        df = df[['Id', 'BusinessUnitId', 'BusinessUnit', 'CompanyId', 'Company', 'DateCreated']]
+        
+        table = "BusinessUnit"
+        Util.import_func(df, table)
+
+    def _import_budget_account(self):
+        try:
+            raw = pd.read_csv(r"./source/BudgetAccount.csv")
+            logging.info("Budget Account data read from CSV.")
+        except Exception as e:
+            logging.exception(f"Error reading Budget Account CSV: {e}")
+            return
+        
+        df = raw.copy()
+        df = df.reset_index(drop=True)
+        df.columns = df.columns.str.replace(' ', "")
+        df['Id'] = df.index + 1
+        
+        table = "Budget_Account"
+        Util.import_func(df, table)
+
+class Util:
+    @staticmethod
+    def import_func(df: pd.DataFrame(), table: str) -> None:
+        try:
+            conn = DBManager().connect()
+            cursor = conn.cursor()
+            cursor.execute(f"DELETE FROM {table}")
+            conn.commit()
+            df.to_sql(table, conn, if_exists="append", index=False)
+            logging.info(f"Data imported into {table} successfully.")
+        except Exception as error:
+            logging.exception(f"Error importing data into {table}: {error}")
+            raise error
+        finally:
+            cursor.close()
+            conn.close()
+            logging.info("Database connection closed.")
+
+    @staticmethod
+    def execute_query(sql: str) -> pd.DataFrame() | None:
+        try:
+            conn = DBManager().connect()
+            df = pd.read_sql(sql, conn)
+            logging.info(f"Data from query:\n{sql}\n successful.")
+            return df
+        except Exception as error:
+            logging.exception(f"Error from query:\n{sql}\n {error}")
+            raise error
+        finally:
+            conn.close()
+            logging.info("Database connection closed.")
+
+    @staticmethod
+    def get_fiscal_year(date):
+        if pd.isna(date):
+            return "UnkownFiscalYear"
+
+        year = date.year
+
+        # Define fiscal year start and end based on the current year
+        fiscal_start = pd.Timestamp(f"{year-1}-10-01")
+        fiscal_end = pd.Timestamp(f"{year}-09-30")
+
+        # If date is between October 1st of the previous year and September 30th of the current year
+        if fiscal_start <= date <= fiscal_end:
+            return f"FY{str(year)[-2:]}"  # Current year as fiscal year
+        else:
+            return f"FY{str(year + 1)[-2:]}"  # Next year as fiscal year
+
+
+if __name__ == "__main__":
+    migration = Migration()
+    migration.import_all()
