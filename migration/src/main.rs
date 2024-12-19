82a6f90ad466d8a2cd0ab4172156849175846e31
Sam Adams <samuel.adams@centralhealth.net>
2024-12-19 15:15:01 -0600

    trying the first version of pulling rows

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,12 +1,27 @@
-use dotenv::dotenv;
 use std::env;
 use tokio_postgres::NoTls;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    dotenv().ok();
+    let database_url = env::var("DATABASE_URL")?
+        .to_string()
+        .as_str();
+    let (mut client, connection) = tokio_postgres::connect(database_url, NoTls).await?;
 
-    let database_url = env::var("DATABASE_URL").unwrap().to_string();
-    let mut client = tokio_postgres::connect(database_url.as_str(), NoTls).await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+    let rows = client.query("SELECT * FROM [user]", &[]).await?;
+
+    for row in rows {
+        let id: String = row.get(0)?;
+        let name: String = row.get(1)?;
+
+        println!("User ID: {}, Name: {}", id, name);
+    }
+
     Ok(())
 }
