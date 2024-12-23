8020c44279033a9a69621e4327726c80f0362c50
Sam Adams <samuel.grant.adams@gmail.com>
2024-12-23 15:55:16 -0600

    got sql script to work

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,24 +1,22 @@
 use std::env;
-use tokio_postgres::NoTls;
+use tokio_postgres::{Connection, NoTls};
 mod schema_write;
 use schema_write::execute_schema_file;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let database_url = env::var("DATABASE_URL")?.to_string();
-    let (client, connection) = tokio_postgres::connect(database_url.as_str(), NoTls).await?;
+    let (client, conn) = tokio_postgres::connect(database_url.as_str(), NoTls).await?;
 
-    println!("Database URL: {}", database_url);
+    // Spawn a blocking task to handle the connection
+    tokio::spawn(async move {
+        if let Err(e) = conn.await {
+            eprintln!("Connection error: {}", e);
+        }
+    });
 
-    match env::current_dir() {
-        Ok(cwd) => println!("Current directory: {:?}", cwd),
-        Err(e) => println!("Error getting current directory: {:?}", e),
-    }
-
-    let file_path = "migration/src/schema/schema.sql";
-    let _ = execute_schema_file(&client, file_path).await;
-
-    println!("made it!");
+    let file_path = "./src/schema_write/schema.sql";
+    execute_schema_file(&client, file_path).await?;
 
     Ok(())
 }
