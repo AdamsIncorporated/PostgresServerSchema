3b35addcf1dd8d03666ceaf2332a0cf7e8b1604b
Sam Adams <samuel.grant.adams@gmail.com>
2024-12-23 09:54:08 -0600

    made schema

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -3,25 +3,16 @@ use tokio_postgres::NoTls;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let database_url = env::var("DATABASE_URL")?
-        .to_string()
-        .as_str();
-    let (mut client, connection) = tokio_postgres::connect(database_url, NoTls).await?;
+    let database_url = env::var("DATABASE_URL")?.to_string();
+    let (client, connection) = tokio_postgres::connect(database_url.as_str(), NoTls).await?;
+
+    println!("Database URL: {}", database_url);
 
     tokio::spawn(async move {
         if let Err(e) = connection.await {
             eprintln!("connection error: {}", e);
         }
     });
-
-    let rows = client.query("SELECT * FROM [user]", &[]).await?;
-
-    for row in rows {
-        let id: String = row.get(0)?;
-        let name: String = row.get(1)?;
-
-        println!("User ID: {}, Name: {}", id, name);
-    }
-
+    
     Ok(())
 }
